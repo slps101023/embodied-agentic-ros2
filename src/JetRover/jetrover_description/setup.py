d248d07b78f69a15be34eac4240e7e9f448f20b4
lihsun <slps101023@gmail.com>
2026-09-11 10:47:48 +0800

    feat(description): add meshes, urdf, rviz, launch and display.launch.py

--- src/JetRover/jetrover_description/setup.py
+++ src/JetRover/jetrover_description/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'jetrover_description'
 
@@ -10,6 +12,18 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.*'))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.*'))),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.*'))),
+        (os.path.join('share', package_name, 'meshes/acker'), glob(os.path.join('meshes/acker', '*.*'))),
+        (os.path.join('share', package_name, 'meshes/arm'), glob(os.path.join('meshes/arm', '*.*'))),
+        (os.path.join('share', package_name, 'meshes/common'), glob(os.path.join('meshes/common', '*.*'))),
+        (os.path.join('share', package_name, 'meshes/gripper'), glob(os.path.join('meshes/gripper', '*.*'))),
+        (os.path.join('share', package_name, 'meshes/mecanum'), glob(os.path.join('meshes/mecanum', '*.*'))),
+        (os.path.join('share', package_name, 'meshes/tank'), glob(os.path.join('meshes/tank', '*.*'))),
+
+
+
     ],
     package_data={'': ['py.typed']},
     install_requires=['setuptools'],
